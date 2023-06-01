40828cbc0ef235a81e7328edb3a65be8f43ad04d
yuval-4241 <yuvalzohar12@gmail.com>
2023-06-01 14:38:31 +0300

    UYUY

--- Assignment4/src/Assignment4.java
+++ Assignment4/src/Assignment4.java
@@ -18,15 +18,32 @@ public class Assignment4 {
 			}
 
 			if(option == 1) { 
+				System.out.println("Please enter a string:");
+				String str=sc.next();
+				System.out.println(uppercaseVowels(str));
+
 							
 			} //check
 
 			if(option == 2) { 
-						
+				System.out.println("Please enter the array length:");
+				int num=sc.nextInt();
+				int []arr=new int [num];
+				System.out.println("Please enter the array values:");
+				for (int i=0;i<arr.length;i++)
+				{
+					arr[i]=sc.nextInt();
+				}
+				System.out.println("Please enter a number to search:");
+				int x=sc.nextInt();;
+				System.out.println( "The number "+x+ " appears "+countOccurrences(arr,x)+  " times in this array ");
 			}
 
 			if(option == 3) { 
-					
+				System.out.println("Please enter str1:");
+				String str1=sc.next();
+				System.out.println("Please enter str2:");
+				String str2=sc.next();	
 			}
 
 			if(option == 4) {
@@ -45,8 +62,42 @@ public class Assignment4 {
 		option = sc.nextInt();
 		return option;
 	}
+	public static String uppercaseVowels(String s) {
+		if(s.length()==0)
+		{
+			return "";
+		}
+		if(s.charAt(0)=='a'|| s.charAt(0)=='e'|| s.charAt(0)=='i'|s.charAt(0)=='o'||s.charAt(0)=='u')	
+		{
+			
+			return (char)((int)s.charAt(0)-32)+uppercaseVowels(s.substring(1));
+		}
+		else
+			return s.charAt(0)+uppercaseVowels(s.substring(1));
+		}
+		
+	
+	public static int countOccurrences(int[] arr, int x) {
+		return countOccurrences(arr,x,arr.length-1);
+	}
+	
+	public static int countOccurrences(int[] arr, int x, int i) {
+		if(i==-1)
+			return 0;
+		if(arr[i]==x)
+			return 1+countOccurrences(arr,x,i=i-1);
+		else
+			return 0+countOccurrences(arr,x,i=i-1);
+	}
+	
 
-}
+
+public static String LCS(String str1, String str2) {
+//if (str1.length()>str2.length())
+//{
+	//CHECK
+
+} 
 
 
 
