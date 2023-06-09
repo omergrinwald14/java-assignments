e30e4611349713a50293f11632fa7869a178b5b2
omergrinwald14 <omergrinwald14@gmail.com>
2023-06-10 00:56:30 +0300

    FINAL

--- Assignment4/src/Assignment4.java
+++ Assignment4/src/Assignment4.java
@@ -18,44 +18,52 @@ public class Assignment4 {
 				System.out.println("Bye Bye!");
 			}
 
-			if(option == 1) { 
+			if(option == 1) { // Upper-case vowels
+				String str;
 				System.out.println("Please enter a string:");
-				String str=sc.next();
-				System.out.println(uppercaseVowels(str));
+				do {
+					str=sc.nextLine(); // user's input
+				}
+				while(str.length()==0);
+				System.out.println(uppercaseVowels(str)); // call the function and print it's result
 			} 
 
-			if(option == 2) { 
+			if(option == 2) { // find how many times 'x' occurres in an array
 				System.out.println("Please enter the array length:");
-				int num=sc.nextInt();
-				int []arr=new int [num];
+				int arrLength=sc.nextInt(); 
+				int []arr=new int [arrLength]; // creating an array in the desired length
 				System.out.println("Please enter the array values:");
 				for (int i=0;i<arr.length;i++)
-					arr[i]=sc.nextInt();
+					arr[i]=sc.nextInt(); // array values from user
 				System.out.println("Please enter a number to search:");
 				int x=sc.nextInt();;
-				System.out.println( "The number "+ x +" appears " + countOccurrences(arr,x) + " times in this array");
+				System.out.println( "The number "+ x +" appears " + countOccurrences(arr,x) + " times in this array"); // printing result
 			}
 
-			if(option == 3) { 
+			if(option == 3) { // longest common sequence
 				String str1;
 				String str2;
 				System.out.println("Please enter str1:");
 				do {
-					str1=sc.nextLine();
+					str1=sc.nextLine();  // input for string 1
 				}
-				while(str1.length() == 0);
+				while(str1.length() == 0); 
 				System.out.println("Please enter str2:");
 				do {
-					str2=sc.nextLine();
+					str2=sc.nextLine(); // input for string 1
 				}
-				while(str2.length()==0);
-				System.out.println("The longest common subsequence is: " + LCS(str1,str2));
+				while(str2.length()==0); 
+				System.out.println("The longest common subsequence is: " + LCS(str1,str2)); // calling the function with the 2 inputs and print result
 			}
 
-			if(option == 4) {
+			if(option == 4) { // count substrings
 				System.out.println("Please enter a string:");
-				String str=sc.next();
-				System.out.println("The number of substrings is " + countSubstrings(str));
+				String str;
+				do {
+					str=sc.nextLine(); // input
+				}
+				while(str.length() == 0);
+				System.out.println("The number of substrings is " + countSubstrings(str)); // calling function and print result
 			}
 		}
 	}	
@@ -71,54 +79,56 @@ public class Assignment4 {
 		option = sc.nextInt();
 		return option;
 	}
-	public static String uppercaseVowels(String s) {
-		if(s.length()==0)
-		{
-			return "";
-		}
-		if(s.charAt(0)=='a'|| s.charAt(0)=='e'|| s.charAt(0)=='i'|s.charAt(0)=='o'||s.charAt(0)=='u')	
-		{
+	public static String uppercaseVowels(String s) { // replace vowels with upper-case letters
+		if(s.length() == 0) 
+			return ""; // base case - return empty string when the string is empty
+		char currentChar = s.charAt((0));
+		if(currentChar == ' ')
+			return ' ' + uppercaseVowels(s.substring(1));
+		if(currentChar=='a'|| currentChar=='e'|| currentChar=='i'||currentChar=='o'||currentChar=='u') // vowels
+			return (char)(currentChar-32) + uppercaseVowels(s.substring(1)); // add upper-case letter and call the function with the rest of the string
 
-			return (char)(s.charAt(0)-32)+uppercaseVowels(s.substring(1));
-		}
 		else
-			return s.charAt(0)+uppercaseVowels(s.substring(1));
+			return currentChar + uppercaseVowels(s.substring(1)); //leave the letter as it is and call the function on the rest of the string
 	}	
-	public static int countOccurrences(int[] arr, int x) {
-		return countOccurrences(arr,x,arr.length-1);
+	public static int countOccurrences(int[] arr, int x) { // count occurrences of 'x' in a certain array of integers
+		return countOccurrences(arr,x,arr.length-1); 
 	}	
 	public static int countOccurrences(int[] arr, int x, int i) {
-		if(i==-1)
-			return 0;
+		if(i<0)
+			return 0; // base case - will get here after we finished the check on index 0
 		if(arr[i]==x)
-			return 1+countOccurrences(arr,x,i=i-1);
+			return 1+countOccurrences(arr,x,i-1); // add one when the value equals x
 		else
-			return 0+countOccurrences(arr,x,i=i-1);
+			return countOccurrences(arr,x,i-1); // call the function and move and index 1 to the left
 	}
-	public static String LCS(String str1, String str2) {
-		if(str1.length()==0 || str2.length()==0)
+	public static String LCS(String str1, String str2) { // longest common sequence
+		if(str1.length()==0 || str2.length()==0) // base case - if one of the strings is empty, there is nothing to compare
 			return "";
-		if(str1.charAt(0)==str2.charAt(0))
-			return str1.substring(0,1) + LCS(str1.substring(1),str2.substring(1));
-		String left = LCS(str1,str2.substring(1));
-		String right = LCS(str1.substring(1),str2);
-		if(left.length()>=right.length())
+		if(str1.charAt(0)==str2.charAt(0)) // the first char at each string is the same
+			return str1.charAt(0) + LCS(str1.substring(1),str2.substring(1)); // return the specific char and add the function with the rest of the strings
+
+		// when it's not stop condition, and the first char is not the same - split into 2 options
+		String left = LCS(str1,str2.substring(1)); // promoting the 2nd string only
+		String right = LCS(str1.substring(1),str2); // promoting the 1st string only
+		if(left.length()>=right.length()) // return the longest sequence
 			return left;
 		return right;
 	}
-	public static int countSubstrings(String str) {
+	public static int countSubstrings(String str) {  // count substrings that begin and end with the same char
 		return countSubstrings(str,0,str.length()-1);
 	}
-	public static int countSubstrings(String str, int start, int end){
-		if (start==str.length()-1)
+	public static int countSubstrings(String str, int start, int end){  
+		if (start==str.length()-1) // base case - a one char substrings
 			return 1;
-		if (end==-1)
-			return countSubstrings(str.substring(1));
-		if(str.charAt(start)==str.charAt(end))
-			return 1 + countSubstrings(str,start,end-1);
+		if (end<0) // after we finished to compare first char to all of the rest
+			return countSubstrings(str.substring(1)); // promote the start index by 1, and start the check again
+		if(str.charAt(start)==str.charAt(end)) // first char equals to another
+			return 1 + countSubstrings(str,start,end-1); // add 1 to the count, move the 'end' index 1 to the left and check again
 		else
-			return countSubstrings(str,start,end-1);
+			return countSubstrings(str,start,end-1); // move the 'end' index 1 to the left and check again
 	}
+
 }
 
 
