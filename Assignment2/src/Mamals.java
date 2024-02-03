7459363090054342dbff83342049adaa9900cce1
yuval-4241 <yuvalzohar12@gmail.com>
2024-02-03 19:13:25 +0200

    j

--- Assignment2/src/Mamals.java
+++ Assignment2/src/Mamals.java
@@ -2,8 +2,10 @@
 public class Mamals {
 String type;
 public Mamals (String type)
+
 {
 	this.type=type;
+	
 }
 	public static boolean forward_player(int i_dest,int j_dest,int i_origin,int j_origin,Mamals [][] check_mamals)
 	{
@@ -12,7 +14,7 @@ public Mamals (String type)
 	            	if(check_mamals[i_dest][j_dest].equals("*"))
 	            	{
 	      
-	            	check_mamals[i_origin][j_origin]="*";
+	            	check_mamals[i_origin][j_origin]."*";
 		 check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
 	            	}
 	                return true;
@@ -22,7 +24,7 @@ public Mamals (String type)
 	        
 	        if (i_origin == i_dest + 2) { //move up
 	            if (j_origin == j_dest - 2)//move up and right
-	            	if(check_mamals[i_origin + 1][j_origin + 1].charAT(1).equals("2"))
+	            	if(check_mamals[i_origin + 1][j_origin + 1].charAt(1).equals("2"))
 	            		check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
 	            	check_mamals[i_origin][j_origin]="*";
 		 check_mamals[i_dest+1][j_dest+1]="*";
@@ -41,7 +43,12 @@ public Mamals (String type)
                 return false;
 	        }
 
-public static boolean is_double_food(int i_dest,int j_dest,int i_origin,int j_origin,Mamals [][] check_mamals) {
-	
+public static boolean is_double_food_forwardright(int i_origin,int j_origin,Mamals [][] check_mamals)// {
+	if(check_mamals[i_origin + 1][j_origin + 1].charAt(1).equals("2"))
+		check_mamals[i_origin+2][j_dest+2]=check_mamals[i_origin][j_origin];
+	check_mamals[i_origin][j_origin]="*";
+check_mamals[i_origin+1][j_origin+1]="*";
+        return true;
+}
 }
 
