45d869de2ddd0db4e4bda3d5c65a5363596f1e68
omergrinwald14 <omergrinwald14@gmail.com>
2024-02-04 18:00:37 +0200

    ss

--- Assignment2/src/Mamals.java
+++ Assignment2/src/Mamals.java
@@ -7,7 +7,7 @@ public class Mamals {
 	public static boolean forward_player(int i_dest,int j_dest,int i_origin,int j_origin,Mamals [][] check_mamals)
 
 	{
-		if (i_origin == i_dest - 1)
+		if (i_origin == i_dest + 1)
 			if (j_origin == j_dest + 1 || j_origin == j_dest - 1)
 				if(check_mamals[i_dest][j_dest].equals("*")){
 					check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
