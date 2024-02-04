fc4db0e91bdc1fbca3381e68dbea9e0e23badb13
omergrinwald14 <omergrinwald14@gmail.com>
2024-02-04 18:32:11 +0200

    a

--- Assignment2/src/Elephant.java
+++ Assignment2/src/Elephant.java
@@ -8,14 +8,28 @@ public class Elephant extends Mamals {
 	public static boolean forward_player(int i_dest,int j_dest,int i_origin,int j_origin,Mamals [][] check_mamals)
 
 	{
-		if (i_origin == i_dest + 1)
-			if (j_origin == j_dest + 1 || j_origin == j_dest - 1)
+		if (i_origin == i_dest+1)
+			if (j_origin == j_dest+1 || j_origin == j_dest-1)
 				if(check_mamals[i_dest][j_dest].equals("*")){
 					check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
 					check_mamals[i_origin][j_origin]=new Mamals("*",0);
 					return true;
 				}
-		if(i)
+		if(i_origin == i_dest+2) {
+			if (j_origin == j_dest+2) // left
+				if(check_mamals[i_dest-1][j_dest-1].equals("*") && check_mamals[i_dest-2][j_dest-2].equals("*")) {
+					check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return true;
+				}
+			if (j_origin == j_dest-2) // right
+				if(check_mamals[i_dest-1][j_dest-1].equals("*") && check_mamals[i_dest-2][j_dest-2].equals("*")) {
+					check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return true;
+				}
+		}	
+				
 		return false;
 	}
 
