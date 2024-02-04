8b555cf9586e50ad4fd17b7a0cdda42cf23d39f1
omergrinwald14 <omergrinwald14@gmail.com>
2024-02-04 19:02:36 +0200

    elephant

--- Assignment2/src/Elephant.java
+++ Assignment2/src/Elephant.java
@@ -4,7 +4,7 @@ public class Elephant extends Mamals {
 	public Elephant(String name,int type) {
 		super(name,type);
 	}
-	
+
 	public static boolean forward_player(int i_dest,int j_dest,int i_origin,int j_origin,Mamals [][] check_mamals)
 
 	{
@@ -29,7 +29,64 @@ public class Elephant extends Mamals {
 					return true;
 				}
 		}	
-				
+
+		return false;
+	}
+	public static boolean forward_computer(int i_origin,int j_origin,Mamals [][] check_mamals)
+
+	{
+		int steps =(int) (1+Math.random());
+		if(steps==1) {
+			if (check_mamals[i_origin+1][j_origin-1].equals("*")||check_mamals[i_origin+1][j_origin+1].equals("*")) {
+				double direction=1+(int)Math.random()*10;
+				if(direction <=5){ //move right
+					check_mamals[i_origin+1][j_origin+1]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return true;
+				}
+				else { //move left 		
+					check_mamals[i_origin+1][j_origin-1]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);	
+					return true;
+				}
+			}
+			if (check_mamals[i_origin+1][j_origin+1].equals("*")) { //only right
+				check_mamals[i_origin+1][j_origin+1]=check_mamals[i_origin][j_origin];
+				check_mamals[i_origin][j_origin]=new Mamals("*",0);
+				return true;
+			}
+			if (check_mamals[i_origin+1][j_origin+1].equals("*")) { //only left
+				check_mamals[i_origin+1][j_origin-1]=check_mamals[i_origin][j_origin];
+				check_mamals[i_origin][j_origin]=new Mamals("*",0);	
+				return true;
+			}
+			steps=2;
+		}
+		if(steps==2) {
+			if (check_mamals[i_origin+2][j_origin-2].equals("*")||check_mamals[i_origin+1][j_origin+1].equals("*")) {
+				double direction=1+(int)Math.random()*10;
+				if(direction <=5 && check_mamals[i_origin+1][j_origin+1].equals("*")){ //move right
+					check_mamals[i_origin+2][j_origin+2]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return true;
+				}
+				else if(check_mamals[i_origin+1][j_origin-1].equals("*")){ //move left 		
+					check_mamals[i_origin+2][j_origin-2]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);	
+					return true;
+				}
+			}
+			if (check_mamals[i_origin+2][j_origin+2].equals("*") && check_mamals[i_origin+1][j_origin+1].equals("*")) { //only right
+				check_mamals[i_origin+2][j_origin+2]=check_mamals[i_origin][j_origin];
+				check_mamals[i_origin][j_origin]=new Mamals("*",0);
+				return true;
+			}
+			if (check_mamals[i_origin+2][j_origin-2].equals("*") && check_mamals[i_origin+1][j_origin-1].equals("*")) { //only left
+				check_mamals[i_origin+2][j_origin-2]=check_mamals[i_origin][j_origin];
+				check_mamals[i_origin][j_origin]=new Mamals("*",0);	
+				return true;
+			}
+		}
 		return false;
 	}
 
