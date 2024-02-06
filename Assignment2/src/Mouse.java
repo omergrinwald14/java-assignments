6050e0acabd279d9fe9eeffed6c1b7cf311689f3
omergrinwald14 <omergrinwald14@gmail.com>
2024-02-06 18:27:29 +0200

    j

--- Assignment2/src/Mouse.java
+++ Assignment2/src/Mouse.java
@@ -1,6 +1,6 @@
 
 public class Mouse extends Mamals {
-
+	protected int PostionBeforeLastEat;
 	public Mouse(String name,int type) {
 		super(name,type);
 	}
@@ -9,7 +9,7 @@ public class Mouse extends Mamals {
 	{
 		if (i_origin == i_dest+1)
 			super.forward_player(i_dest, j_dest, i_origin, j_origin, check_mamals);
-				
+
 		if(i_origin == i_dest+2) {
 			if (j_origin == j_dest+2) // left
 				if(check_mamals[i_dest-1][j_dest-1].equals("*") && check_mamals[i_dest-2][j_dest-2].equals("*")) {
@@ -28,66 +28,66 @@ public class Mouse extends Mamals {
 		return false;
 	}
 	public boolean first_food_player(int i_dest,int j_dest,int i_origin,int j_origin,Mamals [][] check_mamals) {
-		if (i_origin == i_dest - 2) { //move up
+		if (i_origin == i_dest - 2)  //move up
 			if (j_origin == j_dest - 2)//move up and right
 				if(check_mamals[i_origin - 1][j_origin + 1].type==2)
-					if(check_mamals[i_origin - 2][j_origin + 2].type==0)//check that is empty
+					if(check_mamals[i_origin - 2][j_origin + 2].equals("*")) {//check that is empty
 						check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
-			check_mamals[i_origin][j_origin]= new Mamals("*",0);
-
-			return true;
-		}
+						check_mamals[i_origin][j_origin]= new Mamals("*",0);
+						PostionBeforeLastEat=i_origin*10+j_origin;
+						return true;
+					}
 		if (j_origin == j_dest + 2)//up and left
 			if(check_mamals[i_origin - 1][j_origin - 1].type==2)
-				if(check_mamals[i_origin - 2][j_origin - 2].type==0)//check that is empty
-				{//move up and left
+				if(check_mamals[i_origin - 2][j_origin - 2].equals("*")) {//check that is empty
 					check_mamals[i_dest][j_dest]=check_mamals[i_origin][j_origin];
 					check_mamals[i_origin][j_origin]=new Mamals("*",0);
-
-
+					PostionBeforeLastEat=i_origin*10+j_origin;
 					return true;
 				}
-
 		return false;
 	}
 	public String double_food_player(int i_origin,int j_origin,Mamals [][] check_mamals) {
+		int i_cancel=PostionBeforeLastEat/10;
+		int j_cancel=PostionBeforeLastEat%10;
 		if(check_mamals[i_origin - 1][j_origin + 1].type==2)//up right
-			if(check_mamals[i_origin - 2][j_origin + 2].type==0)
-			{
-				check_mamals[i_origin-2][j_origin+2]=check_mamals[i_origin][j_origin];
-				check_mamals[i_origin][j_origin]= new Mamals("*",0);
-				check_mamals[i_origin-1][j_origin+1]=new Mamals("*",0);
-				return "U-R";
+			if(check_mamals[i_origin - 2][j_origin + 2].equals("*")) {
+				if(i_cancel!=i_origin-2||j_cancel!=j_origin+2) {
+					check_mamals[i_origin-2][j_origin+2]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]= new Mamals("*",0);
+					check_mamals[i_origin-1][j_origin+1]=new Mamals("*",0);
+					return "U-R";
+				}
 			}
 		//up and left
 		if(check_mamals[i_origin - 1][j_origin - 1].type==2)
-			if(check_mamals[i_origin - 2][j_origin - 2].type==0)//check that is empty
-			{//move up and left
-				check_mamals[i_origin-2][j_origin-2]=check_mamals[i_origin][j_origin];
-				check_mamals[i_origin][j_origin]=new Mamals("*",0);
-
-				return "U-L";
+			if(check_mamals[i_origin - 2][j_origin - 2].equals("*")){//check that is empty
+				if(i_cancel!=i_origin-2||j_cancel!=j_origin-2) {
+					check_mamals[i_origin-2][j_origin-2]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return "U-L";
+				}
 			}
-		
-		if(check_mamals[i_origin +1][j_origin + 1].type==2)//back right
-			if(check_mamals[i_origin + 2][j_origin + 2].type==0)//check that is empty
-			{//move up and left
-				check_mamals[i_origin+2][j_origin+2]=check_mamals[i_origin][j_origin];
-				check_mamals[i_origin][j_origin]=new Mamals("*",0);
 
-				return "D-R";
+		if(check_mamals[i_origin +1][j_origin + 1].type==2)//down right
+			if(check_mamals[i_origin + 2][j_origin + 2].equals("*")){//check that is empty
+				if(i_cancel!=i_origin-2||j_cancel!=j_origin-2) {
+					check_mamals[i_origin+2][j_origin+2]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return "D-R";
+				}
 			}
-		if(check_mamals[i_origin +1][j_origin - 1].type==2)//back left
-			if(check_mamals[i_origin + 2][j_origin - 2].type==0)//check that is empty
-			{//move up and left
-				check_mamals[i_origin+2][j_origin-2]=check_mamals[i_origin][j_origin];
-				check_mamals[i_origin][j_origin]=new Mamals("*",0);
-
-				return "D-L";
+		if(check_mamals[i_origin +1][j_origin - 1].type==2)//down left
+			if(check_mamals[i_origin + 2][j_origin - 2].equals("*")){//check that is empty
+				if(i_cancel!=i_origin+2||j_cancel!=j_origin-2) {
+					check_mamals[i_origin+2][j_origin-2]=check_mamals[i_origin][j_origin];
+					check_mamals[i_origin][j_origin]=new Mamals("*",0);
+					return "D-L";
+				}
 			}
 		return "";
 	}
-	
+
 	public boolean forward_computer(int i_origin,int j_origin,Mamals [][] check_mamals)
 
 	{
@@ -158,6 +158,7 @@ public class Mouse extends Mamals {
 			{//check that is empty
 				check_mamals[i_origin+2][j_origin+2]=check_mamals[i_origin][j_origin];
 				check_mamals[i_origin][j_origin]= new Mamals("*",0);
+				PostionBeforeLastEat=i_origin*10+j_origin;
 				return true;
 			}
 		if(is_valid_coordinate(i_origin+2, j_origin-2) && check_mamals[i_origin + 1][j_origin - 1].type==1)//move up left
@@ -165,42 +166,47 @@ public class Mouse extends Mamals {
 			{//check that is empty
 				check_mamals[i_origin+2][j_origin-2]=check_mamals[i_origin][j_origin];
 				check_mamals[i_origin][j_origin]= new Mamals("*",0);
+				PostionBeforeLastEat=i_origin*10+j_origin;
 				return true;
 			}
 
 		return false;
 	}
 	public String double_food_computer(int i_origin,int j_origin,Mamals [][] check_mamals) {
-		if(is_valid_coordinate(i_origin-2, j_origin+2) && check_mamals[i_origin - 1][j_origin + 1].type==1)//back right
-			if(check_mamals[i_origin - 2][j_origin + 2].equals("*"))
-			{
+		int i_cancel=PostionBeforeLastEat/10;
+		int j_cancel=PostionBeforeLastEat%10;
+		if(is_valid_coordinate(i_origin-2, j_origin+2) && check_mamals[i_origin - 1][j_origin + 1].type==1)//up right
+			if(check_mamals[i_origin - 2][j_origin + 2].equals("*")) {
+				if(i_cancel!=i_origin-2||j_cancel!=j_origin+2) {
 				check_mamals[i_origin-2][j_origin+2]=check_mamals[i_origin][j_origin];
 				check_mamals[i_origin][j_origin]= new Mamals("*",0);
 				return "D-R";
+				}
 			}
-		//up and left
-		if(is_valid_coordinate(i_origin-2, j_origin-2) && check_mamals[i_origin - 1][j_origin - 1].type==1)//back left
-			if(check_mamals[i_origin - 2][j_origin - 2].equals("*"))//check that is empty
-			{//move up and left
+		if(is_valid_coordinate(i_origin-2, j_origin-2) && check_mamals[i_origin - 1][j_origin - 1].type==1)//up left
+			if(check_mamals[i_origin - 2][j_origin - 2].equals("*")){//check that is empty
+				if(i_cancel!=i_origin-2||j_cancel!=j_origin-2) {
 				check_mamals[i_origin-2][j_origin-2]=check_mamals[i_origin][j_origin];
 				check_mamals[i_origin][j_origin]=new Mamals("*",0);
 				return "D-L";
-				
+				}
 			}
 
-		if(is_valid_coordinate(i_origin+2, j_origin-2) && check_mamals[i_origin +1][j_origin + 1].type==1)//up right
-			if(check_mamals[i_origin + 2][j_origin + 2].equals("*"))//check that is empty
-			{//move up and left
+		if(is_valid_coordinate(i_origin+2, j_origin-2) && check_mamals[i_origin +1][j_origin + 1].type==1)//down right
+			if(check_mamals[i_origin + 2][j_origin + 2].equals("*")){//check that is empty
+				if(i_cancel!=i_origin+2||j_cancel!=j_origin+2) {
 				check_mamals[i_origin+2][j_origin+2]=check_mamals[i_origin][j_origin];
 				check_mamals[i_origin][j_origin]=new Mamals("*",0);
 				return "U-R";
+				}
 			}
-		if(is_valid_coordinate(i_origin+2, j_origin-2) && check_mamals[i_origin +1][j_origin - 1].type==1)//up left
-			if(check_mamals[i_origin + 2][j_origin - 2].equals("*"))//check that is empty
-			{//move up and left
+		if(is_valid_coordinate(i_origin+2, j_origin-2) && check_mamals[i_origin +1][j_origin - 1].type==1)//down left
+			if(check_mamals[i_origin + 2][j_origin - 2].equals("*")){//check that is empty
+				if(i_cancel!=i_origin+2||j_cancel!=j_origin-2) {
 				check_mamals[i_origin+2][j_origin-2]=check_mamals[i_origin][j_origin];
 				check_mamals[i_origin][j_origin]=new Mamals("*",0);
 				return "U-L";
+				}
 			}
 		return "";
 	}
